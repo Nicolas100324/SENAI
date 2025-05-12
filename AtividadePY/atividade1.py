0d7072e1c7e35b0b46ef900f97ff6898a4d862ac
Nicolas100324 <nicolas_merlin@estudante.sesisenai.org.br>
2025-05-12 12:07:39 +0000

    update

--- AtividadePY/atividade1.py
+++ AtividadePY/atividade1.py
@@ -1,9 +1,7 @@
-1. Sanduíches - Quantidade de ingredientes
-python
-CopiarEditar
+sanduiches - Quantidade de ingredientes
+p
 sanduiches = int(input("Quantidade de sanduíches: "))
 queijo = (sanduiches * 2 * 50) / 1000
 presunto = (sanduiches * 1 * 50) / 1000
 hamburguer = (sanduiches * 1 * 100) / 1000
 print(f"Queijo: {queijo}kg, Presunto: {presunto}kg, Hambúrguer: {hamburguer}kg")
-
